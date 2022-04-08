f7308b0fa54f643d452d23ef03bea06b77d6736e
Caio Henrique <caio.henrique.20111@gmail.com>
2022-04-08 18:26:55 -0300

    Enunciado parte 2.

--- ContaBancaria/src/ContaBancaria.java
+++ ContaBancaria/src/ContaBancaria.java
@@ -4,6 +4,17 @@ public class ContaBancaria {
     // Crie um programa/teste que leia dados de contas bancarias de um arquivo texto, uma conta 
     // em casa linha, no formato: Numero, CPF, SaldoInicial
 
+    // O sistema será utilizado por clientes que podem possuir mais de uma conta no banco. Assim,
+    // podemos dizer que o cliente tem uma lista de contas. A conta, por sua vez, tem uma fila das
+    // operações de deposito ou saque que foram realizadas. As tarefas desta etapa, portanto, são:
+
+    // a) Cria as classes cliente e operacao
+    // b) Apos carregar os clientes em um vetor enorme o bastante, carregar as contas. Elas devem ser
+    // carregadas na lista de cada cliente, corretamente, e em uma lista independente contendo todas as contas.
+    // c) Carregar todas as operações para a memoria. Cada operacao deve, entao, ser inserida na fila da sua conta.
+    // d) Melhorar o mini aplicativo para permitir consultar as contas de um cliente e para permitir ver o extrato
+    // de operacoes de uma conta.
+
     int numeroConta;
     long CPFTitular;
     double Saldo;
